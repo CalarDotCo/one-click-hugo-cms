629c106848cddfb9666286a7c7122929caed50b1
CalarDotCo <f3.calarco@gmail.com>
2018-10-14 18:53:43 +0200

    test preview template

--- src/js/cms.js
+++ src/js/cms.js
@@ -12,7 +12,7 @@ import ContactPreview from "./cms-preview-templates/contact";
 class ColorControl extends React.Component {
   render() {
     return <input
-        style={{height: "180px"}}
+        style={{height: "80px"}}
         type="color"
         value={this.props.value}
         onInput={(e) => this.props.onChange(e.target.value)}
@@ -22,7 +22,6 @@ class ColorControl extends React.Component {
 
 CMS.registerPreviewStyle("/css/main.css");
 CMS.registerPreviewTemplate("home", HomePreview);
-CMS.registerPreviewTemplate("post", PostPreview);
 CMS.registerPreviewTemplate("products", ProductsPreview);
 CMS.registerPreviewTemplate("values", ValuesPreview);
 CMS.registerPreviewTemplate("contact", ContactPreview);
