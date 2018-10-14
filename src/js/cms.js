5e6ba0f37d1359d5f9b15efa52eb87bc4236ed0a
CalarDotCo <f3.calarco@gmail.com>
2018-10-14 17:46:05 +0200

    test

--- src/js/cms.js
+++ src/js/cms.js
@@ -12,7 +12,7 @@ import ContactPreview from "./cms-preview-templates/contact";
 class ColorControl extends React.Component {
   render() {
     return <input
-        style={{height: "80px"}}
+        style={{height: "180px"}}
         type="color"
         value={this.props.value}
         onInput={(e) => this.props.onChange(e.target.value)}
